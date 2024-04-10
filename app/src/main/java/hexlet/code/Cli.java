9aee9ba307201307815b4be0f88e67cf718f1bb1
Kseniya Zakharova <2b4got10@mail.ru>
2024-04-10 20:41:28 +0500

    fourth step

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -1,2 +1,16 @@
-package hexlet.code;public class Cli {
+package hexlet.code;
+
+import java.util.Scanner;
+
+public class Cli {
+    public static void sayHi() {
+        System.out.println("Welcome to the Brain Games!");
+        System.out.println("May I have your name?");
+        Scanner scanner = new Scanner(System.in);
+        String userName = scanner.next();
+        System.out.println("Hello, " + userName + "!");
+
+        scanner.close();
+
+    }
 }
