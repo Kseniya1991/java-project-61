6b4f93c716454521d6355d092e13572f9295b8d0
Kseniya Zakharova <2b4got10@mail.ru>
2024-04-12 13:26:58 +0500

    5th step

--- app/src/main/java/hexlet/code/Even.java
+++ app/src/main/java/hexlet/code/Even.java
@@ -1,2 +1,15 @@
-package hexlet.code;public class Even {
+package hexlet.code;
+
+import java.util.Scanner;
+
+public class Even {
+    public static void startEvenGame() {
+        System.out.println("Answer 'yes' if the number is even, otherwise answer 'no'.\n" +
+                "Question: " + (int) (Math.random() * 100));
+        System.out.println("Your answer: ");
+
+        Scanner scanner = new Scanner(System.in);
+        String number = scanner.nextLine();
+
+    }
 }
