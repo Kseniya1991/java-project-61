4eab8e791d5d76b6901f914f270d7e83dbcee426
Kseniya Zakharova <2b4got10@mail.ru>
2024-04-15 01:08:40 +0500

    update class

--- app/src/main/java/hexlet/code/games/EvenOrNotGame.java
+++ app/src/main/java/hexlet/code/games/EvenOrNotGame.java
@@ -30,12 +30,12 @@ public class EvenOrNotGame {
             if ((number % 2 == 0 && answer.equals("no"))) {
                 System.out.println("'no' is wrong answer ;(. Correct answer was 'yes'. "
                         + "\nLet's try again, " + userName + "!");
-                break;
+
             }
             if (number % 2 != 0 && answer.equals("yes")) {
                 System.out.println("'yes' is wrong answer ;(. Correct answer was 'no'. "
                         + "\nLet's try again, " + userName + "!");
-                break;
+
             }
         }
         if (count == 3) {
