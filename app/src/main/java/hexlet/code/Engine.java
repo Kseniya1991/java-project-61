8c7db91bed6bfd0931d18a58eb46b2cb602375a8
Kseniya Zakharova <2b4got10@mail.ru>
2024-04-15 04:30:35 +0500

    added class Engine

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -1,2 +1,13 @@
-package hexlet.code;public class Engine {
+package hexlet.code;
+
+import java.util.Scanner;
+
+public class Engine {
+    public static void run() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome to the Brain Games!");
+        System.out.print("May I have your name? ");
+        String username = scanner.next();
+        System.out.println("Hello, " + username + "!");
+    }
 }
