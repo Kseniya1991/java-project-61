95c20e62b2af51b68ad1fd45c40709798acd3d13
Kseniya Zakharova <2b4got10@mail.ru>
2024-04-15 13:47:26 +0500

    even game with engine

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -3,11 +3,38 @@ package hexlet.code;
 import java.util.Scanner;
 
 public class Engine {
-    public static void run() {
-        Scanner scanner = new Scanner(System.in);
+    private static final int ROUNDS_COUNT = 3; //количество раундов максимально 3
+    public static void run(String rules, String[] questionAnswer) {
+        Scanner scanner = new Scanner(System.in); //приветствие
         System.out.println("Welcome to the Brain Games!");
-        System.out.print("May I have your name? ");
+        System.out.println("May I have your name? ");
         String username = scanner.next();
         System.out.println("Hello, " + username + "!");
+
+        int count = 0;
+
+        while (count != ROUNDS_COUNT) {
+            for (String string : questionAnswer) {
+                System.out.println(rules); //правила
+                System.out.println("Question: " + questionAnswer[0]); //"Question: "
+                System.out.println("Your answer: "); //"Your answer: "
+                String answer = scanner.next();
+
+                if (answer.equals(questionAnswer[1])) {
+                    System.out.println("Correct!");
+                    count++;
+                } else {
+                    System.out.println("'no' is wrong answer ;(. Correct answer was 'yes'. "
+                            + "\nLet's try again, " + username + "!");
+                    break;
+                }
+                System.out.println(count);
+            }
+        }
+        if (count == ROUNDS_COUNT) {
+            System.out.println("Congratulations, " + username + "!");
+        }
     }
 }
+
+
--- app/src/main/java/hexlet/code/games/EvenOrNotGame.java
+++ app/src/main/java/hexlet/code/games/EvenOrNotGame.java
@@ -1,46 +1,27 @@
 package hexlet.code.games;
 
+import hexlet.code.Engine;
+
 import java.util.Scanner;
 
 public class EvenOrNotGame {
     public static void startEvenGame() {
-        System.out.println("Welcome to the Brain Games!\n" + "May I have your name?");
-        Scanner scanner = new Scanner(System.in);
-        String username = scanner.next();
-        System.out.println("Hello, " + username + "!");
 
-        int count = 0;
-        while (count < 3) {
-            int number = (int) (Math.random() * 100);
-            System.out.println("Answer 'yes' if the number is even, otherwise answer 'no'.");
-            System.out.println("Question: " + number);
-            System.out.println("Your answer: ");
-            String answer = scanner.nextLine();
+        String rules = "Answer 'yes' if the number is even, otherwise answer 'no'."; //правила
+        String[] questionAnswer = createQuestionAndAnswer(); //вопрос-ответ
+        Engine.run(rules, questionAnswer);
+    }
 
-            if (!(answer.equals("no")) && !(answer.equals("yes"))) {
-                System.out.println(answer + " is wrong answer ;(. \nLet's try again, " + username + "!");
-            }
+    public static String[] createQuestionAndAnswer() {
+        String[] result = new String[2];
+        int number = (int) (Math.random() * 100);
+        result[0] = String.valueOf(number); //question
 
-            if ((number % 2 == 0 && answer.equals("yes")) || number % 2 != 0 && answer.equals("no")) {
-                System.out.println("Correct!");
-                count++;
-            }
-
-            if ((number % 2 == 0 && answer.equals("no"))) {
-                System.out.println("'no' is wrong answer ;(. Correct answer was 'yes'. "
-                        + "\nLet's try again, " + username + "!");
-                break;
-            }
-
-            if (number % 2 != 0 && answer.equals("yes")) {
-                System.out.println("'yes' is wrong answer ;(. Correct answer was 'no'. "
-                        + "\nLet's try again, " + username + "!");
-                break;
-            }
+        if (number % 2 == 0) {
+            result[1] = "yes";
+        } else {
+            result[1] = "no";
         }
-
-        if (count == 3) {
-            System.out.println("Congratulations, " + username + "!");
-        }
+        return result;
     }
 }
