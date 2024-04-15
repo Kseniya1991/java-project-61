df5e9f52e411ed125348e5ee45e166ecdb9078c8
Kseniya Zakharova <2b4got10@mail.ru>
2024-04-15 16:42:59 +0500

    deleted print line

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -6,9 +6,9 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Engine {
-    public static final int ROUNDS_COUNT = 3; //количество раундов максимально 3
+    public static final int ROUNDS_COUNT = 3;
     public static void run(String rules, String[][] questionAnswer) {
-        Scanner scanner = new Scanner(System.in); //приветствие
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Welcome to the Brain Games!");
         System.out.println("May I have your name? ");
         String username = scanner.next();
@@ -17,17 +17,12 @@ public class Engine {
         int count = 0;
         while (count != ROUNDS_COUNT) {
             System.out.println(rules); //правила
-            System.out.println(Arrays.toString(questionAnswer[count])); //показать сгенерированный массив условий
+
+            //System.out.println(Arrays.toString(questionAnswer[count])); //показать сгенерированный массив условий
 
             System.out.println("Question: " + questionAnswer[count][0]);
             System.out.println("Your answer: ");
             String answer = scanner.next();
-
-            if (!Arrays.asList(questionAnswer[count]).contains(answer)) {
-                System.out.println(answer + " is wrong answer ;(."
-                        + "\nLet's try again, " + username + "!");
-                break;
-            }
 
             if (answer.equals(questionAnswer[count][1])) {
                 System.out.println("Correct!");
@@ -38,6 +33,12 @@ public class Engine {
                         + "\nLet's try again, " + username + "!");
                 break;
             }
+
+            /*if (!Arrays.asList(questionAnswer[count]).contains(answer)) {
+                System.out.println(answer + " is wrong answer ;(."
+                        + "\nLet's try again, " + username + "!");
+                break;
+            } */
         }
         if (count == ROUNDS_COUNT) {
             System.out.println("Congratulations, " + username + "!");
