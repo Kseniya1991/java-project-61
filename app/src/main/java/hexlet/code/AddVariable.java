6d1e854cc4fde5ec8706e6eae4601ce1701f528f
Kseniya Zakharova <2b4got10@mail.ru>
2024-04-17 02:22:41 +0500

    added '{} for 'if'

--- app/src/main/java/hexlet/code/AddVariable.java
+++ app/src/main/java/hexlet/code/AddVariable.java
@@ -1,6 +1,10 @@
 package hexlet.code;
 
-import hexlet.code.games.*;
+import hexlet.code.games.EvenOrNotGame;
+import hexlet.code.games.CalculatorGame;
+import hexlet.code.games.GreatestCommonDivisorGame;
+import hexlet.code.games.ProgressionGame;
+import hexlet.code.games.PrimeNumberGame;
 
 public class AddVariable {
     public static String createRules(String rules) {
@@ -10,16 +14,21 @@ public class AddVariable {
     public static String[][] createQuestionAnswerArray(int gameId) {
         String[][] questionAnswer = new String[Engine.ROUNDS_COUNT][2];
             for (int i = 0; i < Engine.ROUNDS_COUNT; i++) {
-                if (gameId == EvenOrNotGame.MENU_ITEM)
+                if (gameId == EvenOrNotGame.MENU_ITEM) {
                     questionAnswer[i] = EvenOrNotGame.createQuestionAndAnswer();
-                if (gameId == CalculatorGame.MENU_ITEM)
+                }
+                if (gameId == CalculatorGame.MENU_ITEM) {
                     questionAnswer[i] = CalculatorGame.createQuestionAndAnswer();
-                if (gameId == GreatestCommonDivisorGame.MENU_ITEM)
+                }
+                if (gameId == GreatestCommonDivisorGame.MENU_ITEM) {
                     questionAnswer[i] = GreatestCommonDivisorGame.createQuestionAndAnswer();
-                if (gameId == ProgressionGame.MENU_ITEM)
+                }
+                if (gameId == ProgressionGame.MENU_ITEM) {
                     questionAnswer[i] = ProgressionGame.createQuestionAndAnswer();
-                if (gameId == PrimeNumberGame.MENU_ITEM)
+                }
+                if (gameId == PrimeNumberGame.MENU_ITEM) {
                     questionAnswer[i] = PrimeNumberGame.createQuestionAndAnswer();
+                }
         }
             return questionAnswer;
     }
