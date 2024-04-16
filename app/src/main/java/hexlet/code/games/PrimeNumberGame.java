e12fe1f32a40d94a59eec4a717791dd1f154acc2
Kseniya Zakharova <2b4got10@mail.ru>
2024-04-16 15:25:49 +0500

    prime game with engine

--- app/src/main/java/hexlet/code/games/PrimeNumberGame.java
+++ app/src/main/java/hexlet/code/games/PrimeNumberGame.java
@@ -1,55 +1,40 @@
 package hexlet.code.games;
 
-import java.util.Scanner;
+import hexlet.code.Engine;
 
 public class PrimeNumberGame {
     public static void startPrimeNumberGame() {
-        /*System.out.println("Welcome to the Brain Games!\n" + "May I have your name?");
+        String rules = "Answer 'yes' if given number is prime. Otherwise answer 'no'.";
+        String[][] questionAnswer = new String[Engine.ROUNDS_COUNT][2];
 
-        System.out.println("Hello, " + username + "!");*/
+        for (int i = 0; i < Engine.ROUNDS_COUNT; i++) {
+            questionAnswer[i] = createQuestionAndAnswer();
+        }
 
+        Engine.run(rules, questionAnswer);
+    }
 
-        Scanner scanner = new Scanner(System.in);
-        String username = scanner.next();
+    public static String[] createQuestionAndAnswer() {
+        String[] result = new String[2]; //массив вопрос-ответ
+        int number = (int) (Math.random() * 100);
+        result[0] = String.valueOf(number);
 
-        int count = 0;
-        while (count < 3) {
-            int number = (int) (Math.random() * 100);
-            String result = "";
-            System.out.println("Answer 'yes' if given number is prime. Otherwise answer 'no'.");
-            System.out.println("Question: " + number);
+        if (number < 2) {
+            result[1] = "no";
+        }
 
-            if (number < 2) {
-                result = "no";
-            }
-
-            int countOfDividers = 3;
-            int counter = 0;
-            for (int i = 1; i <= number; i++) {
-                if (number % i == 0) {
-                    counter++;
-                }
-            }
-
-            if (count >= countOfDividers) {
-                result = "no";
-            }
-
-            String answer = scanner.next();
-            System.out.println("Your answer: " + answer);
-
-            if (answer.equals(result)) {
-                System.out.println("Correct!");
-                count++;
-            } else {
-                System.out.println("'" + answer + "'" + " is wrong answer ;(. "
-                        + "Correct answer was '" + result + "'. \nLet's try again, " + username + "!");
-                break;
+        int countOfDividers = 3;
+        int counter = 0;
+        for (int i = 1; i <= number; i++) {
+            if (number % i == 0) {
+                counter++;
             }
         }
 
-        if (count == 3) {
-            System.out.println("Congratulations, " + username + "!");
+        if (counter >= countOfDividers) {
+            result[1] = "no";
         }
+
+        return result;
     }
 }
