d38028cbac214540247d8dbc77b215f4f3eb6a37
Kseniya Zakharova <2b4got10@mail.ru>
2024-04-16 22:29:21 +0500

    upd progression game

--- app/src/main/java/hexlet/code/games/ProgressionGame.java
+++ app/src/main/java/hexlet/code/games/ProgressionGame.java
@@ -19,32 +19,52 @@ public class ProgressionGame {
     public static String[] createQuestionAndAnswer() {
         String[] result = new String[2]; //массив вопрос-ответ
         int progressionLength = 10;
-        int hiddenNumberIndex = (int) (Math.random() * progressionLength - 1);
         int begin = (int) (Math.random() * Engine.NUMBER_RANGE); //начальное число, исключая ноль
         int summWith = 1 + (int) (Math.random() * 10); //число, на которое увеличивают предыдущее
         int[] progression = new int[progressionLength]; //массив вопрос, список чисел
 
-        for (int i = 0; i < progression.length; i++) {
-            if (i == 0) {
-                progression[0] = begin;
-            } else {
-                progression[i] = progression[i - 1] + summWith;
-            }
-        }
-
-        int answer = progression[hiddenNumberIndex]; //задаем число-ответ
-        result[1] = String.valueOf(answer);
-        result[0] = String.valueOf(begin);
-
         for (int i = 1; i < progression.length; i++) {
-            result[0] += " ";
-            if (i == hiddenNumberIndex) {
-                result[0] += "..";
+            progression[i] = progression[i - 1] + summWith;
+        }
+        int answer = progression[(int) (Math.random() * 9)]; //задаем число-ответ
+        result[1] = answer + "";
+        String question = "";
+        for (int i = 1; i < progression.length; i++) {
+            if (progression[i] == answer) {
+                question += ".. ";
             } else {
-                result[0] += String.valueOf(progression[i]);
+                question += progression[i] + " ";
             }
         }
-
+        result[0] = question;
         return result;
     }
+}
+
+public static String[] createQuestionAndAnswer() {
+    String[] result = new String[2]; //массив вопрос-ответ
+    int[] progression = new int[10]; //массив вопрос, список чисел
+    int begin = 1 + (int) (Math.random() * 10); //начальное число, исключая ноль
+    int summWith = (int) (Math.random() * 10); //число, на которое увеличивают предыдущее
+    progression[0] = begin; //начальное число в массиве
+    int begin = (int) (Math.random() * 10); //начальное число, исключая ноль
+    int summWith = 1 + (int) (Math.random() * 10); //число, на которое увеличивают предыдущее
+    //progression[0] = begin; //начальное число в массиве
+
+    for (int i = 1; i < progression.length; i++) {
+        progression[i] = progression[i - 1] + summWith;
+    }
+    int answer = progression[(int) (Math.random() * 9)]; //задаем число-ответ
+    result[1] = answer + "";
+    String question = "";
+    for (int i = 1; i < progression.length; i++) {
+        if (progression[i] == answer) {
+            question += ".. ";
+        } else {
+            question += progression[i] + " ";
+        }
+    }
+    result[0] = question;
+    return result;
+}
 }
